0a91b9a6be05c77b3c4bba04a733f2b58c8447ad
1CodeLearner <51289481+1CodeLearner@users.noreply.github.com>
2024-01-12 13:24:30 +0900

    Selection sort Algorithm

---
+++ Algorithm_Implementation/Algorithm_Implementation.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int unsortedInt[] = { 1, 6,3, 5, 7, 1,8, 12,4, 6 }; //7, 7,7, 2,3,4, 6,7,7, 8,6,8,9,0 ,1, 2,12,6};
+int size = 10;
+
+
+
+void SelectionSortExample(int* _ptrArr, int _size)
+{
+	//max value first
+	for (int i = 0; i < _size - 1; ++i)
+	{
+		int value = _ptrArr[i];
+		int valueIndex = i;
+		for (int j = i + 1; j < _size; ++j)
+		{
+			if (value < _ptrArr[j])
+			{
+				value = _ptrArr[j];
+				valueIndex = j;
+			}
+		}
+		int temp = _ptrArr[i];
+		_ptrArr[i] = value;
+		_ptrArr[valueIndex] = temp;
+	}
+	for (int i = 0; i < _size; ++i) 
+	{
+		std::cout << _ptrArr[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	SelectionSortExample(unsortedInt, size);
+}
